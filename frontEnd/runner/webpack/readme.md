0d21d050e7d175dac73da3f47c8626ecd1ff3207
dstyle0210 <dstyle0210@gmail.com>
2021-03-04 18:20:28 +0900

    webpack 기초자료 등록

--- frontEnd/runner/webpack/readme.md
+++ frontEnd/runner/webpack/readme.md
@@ -20,3 +20,18 @@ npm i webpack webpack-cli -d
 npm i -D @babel/core babel-loader @babel/preset-env
 ```
 
+#### ejs 설치
+```
+npm i html-webpack-plugin html-loader -D
+```
+
+#### style , css , scss 설치
+```
+npm i css-loader style-loader sass-loader -D
+```
+
+#### scss -> css 외부로 생성
+```
+npm i mini-css-extract-plugin -D
+```
+
--- frontEnd/runner/webpack/webpack.config.js
+++ frontEnd/runner/webpack/webpack.config.js
@@ -1,14 +1,51 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     entry:{
-        index:["./src/main.js","./src/common.js"],
-        app:"./src/app.js"
+        app:"./src/app.js",
+        scss:"./src/sass.js"
     },
     output: {
-        filename: "[name].[chunkhash].js",
+        // filename: "[name].[chunkhash].js",
+        filename: "[name].bundle.js",
         path:__dirname + "/dist"
     },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title: 'Hello Webpack Project!',
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            template: './src/index.ejs'
+        }),
+        new MiniCssExtractPlugin()
+    ],
     module:{
         rules:[
+            {
+                test:/\.css$/,
+                use:["style-loader","css-loader"],
+                exclude:/mode_modules/
+            },
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader,"css-loader","sass-loader"]
+            },
+            /*
+            // scss 파일을 css로 떨굴때
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader,"css-loader","sass-loader"]
+            },
+            // scss 파일을 html 안에 style로 넣을때
+            {
+                test:/\.scss$/,
+                use:["style-loader","css-loader","sass-loader"],
+                exclude:/mode_modules/
+            },
+             */
             {
                 test:/\.js?$/,
                 use:{
