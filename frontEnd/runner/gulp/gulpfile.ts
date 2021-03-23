d69a6e17fed03100036bcb8f234bf28dd3385d03
dstyle0210 <dstyle0210@gmail.com>
2021-03-23 14:24:59 +0900

    gulp rev 등록 후 재지정 처리

--- frontEnd/runner/gulp/gulpfile.ts
+++ frontEnd/runner/gulp/gulpfile.ts
@@ -15,40 +15,62 @@ const replace = require('gulp-replace');
 const csscomb = require('gulp-csscomb');
 const order = require('gulp-order');
 
-
-function sassCompile() {
-    return src('./scss/**/*.scss', { since: lastRun(sassCompile) })
+function sassDevCompile() {
+    return src("./src/_entry/**/*.scss")
         .pipe(sass().on('error', sass.logError))
         .pipe(csscomb("./zen.json"))
-        .pipe(order())
+        .pipe(cleanCSS())
+        .pipe(replace(/}/g,'}\n'))
+        .pipe(replace('/*!','\n/*!'))
+        .pipe(replace('*/','*/\n'))
+        .pipe(replace('{.','{\n\s.')) // 미디어쿼리 일경우, 줄바꿈 처리
+        .pipe(replace('"UTF-8";','"UTF-8";\n'))
+        .pipe(replace(/\}\n\}/g,'} }'))
+        .pipe(replace(/\}\nto{/g,'} to {'))
+        .pipe(replace(/\n(\.[0-9]+)\)/g,'$1)'))
+        .pipe(dest('./src/css'))
+}
+function sassDist() {
+    var map = [
+        "./src/css/base.css",
+        "./src/css/atom.css",
+    ];
+    return src(map) // , { since: lastRun(sassCompile) }
         .pipe(concat("style.css"))
+        .pipe(replace('/*!','/*'))
         .pipe(cleanCSS({debug: true}, (details) => {
             console.log(`${details.name}: ${details.stats.originalSize}`);
             console.log(`${details.name}: ${details.stats.minifiedSize}`);
         }))
-        .pipe(dest('./css/'))
+        .pipe(dest('./dist/css/'))
         .pipe(rev())
         .pipe(override())
-        .pipe(dest('./css/build/'))
+        .pipe(dest('./dist/css/build/'))
         .pipe(rev.manifest())
-        .pipe(dest('./css/'))
+        .pipe(dest('./dist/css/'))
 };
 function sassWatch() {
-    return watch('./scss/**/*.scss', sassCompile);
+    return watch('./src/**/*.scss', sassDevCompile);
 };
 // 변경된 파일에 대한 최종 해쉬값 적용
-function flagHash(){
-    var manifest = fs.readFileSync("./css/rev-manifest.json","utf8");
-    var hash = "/*"+( (manifest.trim()).replace(/[\s\n]/ig,"") )+"*/";
-    return src('./css/style.css')
+function checkRev(){
+    var manifestText = fs.readFileSync("./dist/css/rev-manifest.json","utf8");
+    manifestText = manifestText.replace(/(\.css)/g,"");
+    var manifestData = JSON.parse( manifestText );
+
+    var hash = "/* "+( manifestData.style )+" */";
+    return src('./dist/css/style.css')
+        // .pipe(replace("}","}\n"))
         .pipe(insert.prepend( hash ))
         .pipe(replace("@charset","\n@charset"))
-        .pipe(replace("}","}\n"))
-        .pipe(dest('./css/'));
+        // .pipe(replace('"UTF-8";','"UTF-8";\n'))
+        .pipe(dest('./dist/css/'));
 }
 
-exports.sass = series(sassCompile,flagHash);
-exports.sass_watch = series(sassCompile,sassWatch);
+exports.dev = series(sassDevCompile);
+exports.dist = series(sassDevCompile , sassDist , checkRev);
+/*exports.sass = series(sassCompile,flagHash);
+exports.sass_watch = series(sassCompile,sassWatch);*/
 
 
 /*
