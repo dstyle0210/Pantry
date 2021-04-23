f7fb7736292a07a85839be1c94de2ce8633fa2af
dstyle0210 <dstyle0210@gmail.com>
2021-04-23 18:06:12 +0900

    svelte

--- mypet/stock/Stock.svelte
+++ mypet/stock/Stock.svelte
@@ -7,6 +7,7 @@
 <table>
     <tr>
         <th>이름</th>
+        <th>링크</th>
         <th>컨센서스</th>
         <th>종가</th>
         <th>적정주가</th>
@@ -19,9 +20,12 @@
     <tr>
         <td>
             <a href="https://navercomp.wisereport.co.kr/v2/company/c1010001.aspx?cmp_cd={stock.sNo}">{stock.name}</a>
+        </td>
+        <td>
             <a href="https://finance.naver.com/item/main.nhn?code={stock.sNo}">[N]</a>
             <a href="https://dart.fss.or.kr/html/search/SearchCompany_M2.html?textCrpNM={stock.sNo}">[Dart]</a>
             <a href="http://www.thebell.co.kr/free/content/Search.asp?page=&amp;sdt=&amp;period=180&amp;part=T&amp;keyword={stock.name}">[theBell]</a>
+            <a href="http://comp.fnguide.com/SVO2/asp/SVD_Main.asp?pGB=1&gicode=A{stock.sNo}">[Wise]</a>
         </td>
         <td>{stock.consensus}억</td>
         <td>{stock.stockPrice}원</td>
